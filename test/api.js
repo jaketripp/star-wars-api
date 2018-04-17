d80d7a9c9046248cb7d2fbe3055b2b7674932511
Jacob Tripp <jake@Shimmer.domain>
2018-04-16 18:15:22 -0600

    Test that API returns JSON

--- test/api.js
+++ test/api.js
@@ -9,19 +9,23 @@ describe("api endpoints", function () {
     it('should be able to fetch person #1', function () {
       axios
         .get(`${baseUrl}/people/1`)
-        .then(({ data }) => {
+        .then(response => {
+          let { data } = response;
+          expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
           expect(data.name).to.be.equal("Luke Skywalker");
           expect(data.name).to.not.equal("Star Wars");
         });
     });
-    
+
     it('should be able to fetch the first 10 people', function () {
       axios
         .get(`${baseUrl}/people`)
-        .then(({ data }) => {
+        .then(response => {
+          let { data } = response;
           var people = data.results.map((person) => {
             return person.name;
           });
+          expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
           expect(people).to.be.an('array').that.includes('C-3PO');
           expect(people).to.be.an('array').that.does.not.include('C-3POzzz');
         });
@@ -32,7 +36,9 @@ describe("api endpoints", function () {
     it('should be able to fetch planet #1', function () {
       axios
         .get(`${baseUrl}/planets/1`)
-        .then(({ data }) => {
+        .then(response => {
+          let { data } = response;
+          expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
           expect(data.name).to.be.equal("Tatooine");
           expect(data.name).to.not.equal("Star Wars");
         });
@@ -41,13 +47,16 @@ describe("api endpoints", function () {
     it('should be able to fetch the first 10 planets', function () {
       axios
         .get(`${baseUrl}/planets`)
-        .then(({ data }) => {
+        .then(response => {
+          let { data } = response;
           var planets = data.results.map((planet) => {
             return planet.name;
           });
+          expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
           expect(planets).to.be.an('array').that.includes("Hoth");
           expect(planets).to.be.an('array').that.does.not.include("Hoath");
         });
     });
-  })
+  });
+
 });
