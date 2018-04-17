0077f679e614705a124e71bb06860acc7b87d1c8
Jacob Tripp <jake@Shimmer.domain>
2018-04-16 23:28:37 -0600

    Test sort functionality for /planets

--- test/api.js
+++ test/api.js
@@ -24,7 +24,7 @@ describe("api endpoints", function () {
         .get(`${baseUrl}/people`)
         .then(response => {
           let { data } = response;
-          var people = data.results.map((person) => {
+          let people = data.results.map((person) => {
             return person.name;
           });
           expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
@@ -35,21 +35,21 @@ describe("api endpoints", function () {
         });
     });
 
+    // ['name', 'mass', 'height']
     describe('sorting', function () {
 
-      // ['name', 'mass', 'height']
       it('should be able to sort the first 10 people by name alphabetically', function () {
         return axios
           .get(`${baseUrl}/people/?sort=name`)
           .then(response => {
             let { data } = response;
-            var people = data.results.map((person) => {
+            let people = data.results.map((person) => {
               return person.name;
             });
             // ["Beru Whitesun lars", "Biggs Darklighter", "C-3PO", "Darth Vader", "Leia Organa", "Luke Skywalker", "Obi-Wan Kenobi", "Owen Lars", "R2-D2", "R5-D4"]
             // copy the array, sort it
             // then compare it to itself
-            var sortedPeople = people.slice(0).sort();
+            let sortedPeople = people.slice(0).sort();
             expect(sortedPeople).to.eql(people);
             expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
           }).catch(() => {
@@ -61,7 +61,7 @@ describe("api endpoints", function () {
         return axios
           .get(`${baseUrl}/people/?sort=mass`)
           .then(response => {
-            var people = response.data.results.map((person) => {
+            let people = response.data.results.map((person) => {
               return person.name;
             });
             let sortedPeople = ['R2-D2',
@@ -86,7 +86,7 @@ describe("api endpoints", function () {
         return axios
           .get(`${baseUrl}/people/?sort=height`)
           .then(response => {
-            var people = response.data.results.map((person) => {
+            let people = response.data.results.map((person) => {
               return person.name;
             });
             let sortedPeople = ['R2-D2',
@@ -103,7 +103,7 @@ describe("api endpoints", function () {
             expect(sortedPeople).to.eql(people);
             expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
           }).catch(() => {
-            throw new Error("Not sorted by mass");
+            throw new Error("Not sorted by height");
           });
       });
     });
@@ -129,7 +129,7 @@ describe("api endpoints", function () {
         .get(`${baseUrl}/planets`)
         .then(response => {
           let { data } = response;
-          var planets = data.results.map((planet) => {
+          let planets = data.results.map((planet) => {
             return planet.name;
           });
           expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
@@ -138,6 +138,81 @@ describe("api endpoints", function () {
         }).catch(() => {
           throw new Error("Unable to fetch first 10 planets");
         });
+    });
+
+    // ['name', 'population', 'diameter']
+    describe('sorting', function () {
+
+      it('should be able to sort the first 10 planets by name alphabetically', function () {
+        return axios
+          .get(`${baseUrl}/planets/?sort=name`)
+          .then(response => {
+            let { data } = response;
+            let planets = data.results.map((planet) => {
+              return planet.name;
+            });
+            // ['Alderaan', 'Bespin', 'Coruscant', 'Dagobah', 'Endor', 'Geonosis', 'Hoth', 'Kamino', 'Naboo', 'Yavin IV']
+            // copy the array, sort it
+            // then compare it to itself
+            let sortedPlanets = planets.slice(0).sort();
+            expect(sortedPlanets).to.eql(planets);
+            expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
+          }).catch(() => {
+            throw new Error("Not sorted by name");
+          });
+      });
+
+      it('should be able to sort the first 10 planets by population', function () {
+        return axios
+          .get(`${baseUrl}/planets/?sort=population`)
+          .then(response => {
+            let { data } = response;
+            let planets = data.results.map((planet) => {
+              return planet.name;
+            });
+            let sortedPlanets = ['Yavin IV',
+              'Bespin',
+              'Endor',
+              'Kamino',
+              'Alderaan',
+              'Naboo',
+              'Geonosis',
+              'Coruscant',
+              'Hoth',
+              'Dagobah'
+            ];
+            expect(sortedPlanets).to.eql(planets);
+            expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
+          }).catch(() => {
+            throw new Error("Not sorted by population");
+          });
+      });
+
+      it('should be able to sort the first 10 planets by diameter', function () {
+        return axios
+          .get(`${baseUrl}/planets/?sort=diameter`)
+          .then(response => {
+            let { data } = response;
+            let planets = data.results.map((planet) => {
+              return planet.name;
+            });
+            let sortedPlanets = ['Endor',
+              'Hoth',
+              'Dagobah',
+              'Yavin IV',
+              'Geonosis',
+              'Naboo',
+              'Coruscant',
+              'Alderaan',
+              'Kamino',
+              'Bespin'
+            ];
+            expect(sortedPlanets).to.eql(planets);
+            expect(response.headers['content-type']).to.equal('application/json; charset=utf-8');
+          }).catch(() => {
+            throw new Error("Not sorted by diameter");
+          });
+      });
     });
   });
 
