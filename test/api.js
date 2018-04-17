3b5ef4f0bf9bc8eb4b02602c85d2f3d63551b1d2
Jacob Tripp <jake@Shimmer.domain>
2018-04-17 13:19:19 -0600

    Refactor tests to use 'done' and update tests for /planets and /people

--- test/api.js
+++ test/api.js
@@ -5,23 +5,23 @@ let baseUrl = "http://localhost:3000";
 
 describe("api endpoints", function() {
   describe("people", function() {
-    it("should be able to fetch person #1", function() {
-      return axios
+    it("should be able to fetch person #1", function(done) {
+      axios
         .get(`${baseUrl}/people/1`)
         .then(response => {
           let { data } = response;
-          expect(response.headers["content-type"]).to.equal(
-            "application/json; charset=utf-8"
-          );
+          expect(response.headers["content-type"]).to.equal("application/json; charset=utf-8");
           expect(data.name).to.be.equal("Luke Skywalker");
           expect(data.name).to.not.equal("Star Wars");
+          done()
         })
-        .catch(() => {
-          throw new Error("Unable to fetch person 1");
+        .catch(e => {
+          console.log(e);
+          done(e);
         });
     });
 
-    it("should be able to fetch the first 10 people", function() {
+    it("should be able to fetch all the people", function(done) {
       axios
         .get(`${baseUrl}/people`)
         .then(response => {
@@ -29,105 +29,86 @@ describe("api endpoints", function() {
           let people = data.results.map(person => {
             return person.name;
           });
-          expect(response.headers["content-type"]).to.equal(
-            "application/json; charset=utf-8"
-          );
+          expect(response.headers["content-type"]).to.equal("application/json; charset=utf-8");
           expect(people)
             .to.be.an("array")
             .that.includes("C-3PO");
           expect(people)
             .to.be.an("array")
             .that.does.not.include("C-3POzzz");
+          done();
         })
-        .catch(() => {
-          throw new Error("Unable to fetch first 10 people");
+        .catch(e => {
+          console.log(e);
+          done(e);
         });
     });
 
     // ['name', 'mass', 'height']
     describe("sorting", function() {
-      it("should be able to sort the first 10 people by name alphabetically", function() {
-        return axios
+      it("should be able to sort the first 10 people by name alphabetically", function(done) {
+        axios
           .get(`${baseUrl}/people/?sort=name`)
           .then(response => {
             let { data } = response;
             let people = data.results.map(person => {
               return person.name;
-            });
-            // ["Beru Whitesun lars", "Biggs Darklighter", "C-3PO", "Darth Vader", "Leia Organa", "Luke Skywalker", "Obi-Wan Kenobi", "Owen Lars", "R2-D2", "R5-D4"]
+            }).slice(0,10);
             // copy the array, sort it
             // then compare it to original
             let sortedPeople = people.slice(0).sort();
             expect(sortedPeople).to.eql(people);
-            expect(response.headers["content-type"]).to.equal(
-              "application/json; charset=utf-8"
-            );
+            expect(response.headers["content-type"]).to.equal("application/json; charset=utf-8");
+            done();
           })
-          .catch(() => {
-            throw new Error("Not sorted by name");
+          .catch(e => {
+            console.log(e);
+            done(e);
           });
       });
 
-      it("should be able to sort the first 10 people by mass", function() {
-        return axios
+      it("should be able to sort the first 10 people by mass", function(done) {
+        axios
           .get(`${baseUrl}/people/?sort=mass`)
           .then(response => {
             let people = response.data.results.map(person => {
               return person.name;
-            });
-            let sortedPeople = [
-              "R2-D2",
-              "R5-D4",
-              "Leia Organa",
-              "C-3PO",
-              "Beru Whitesun lars",
-              "Luke Skywalker",
-              "Obi-Wan Kenobi",
-              "Biggs Darklighter",
-              "Owen Lars",
-              "Darth Vader"
-            ];
+            }).slice(0,10);
+
+            let sortedPeople = ["Padmé Amidala", "Jabba Desilijic Tiure", "Sly Moore", "Leia Organa", "Wat Tambor", "Ratts Tyerell", "R5-D4", "R2-D2", "Adi Gallia", "Dud Bolt"];
+
             expect(sortedPeople).to.eql(people);
-            expect(response.headers["content-type"]).to.equal(
-              "application/json; charset=utf-8"
-            );
+            expect(response.headers["content-type"]).to.equal("application/json; charset=utf-8");
+            done();
           })
-          .catch(() => {
-            throw new Error("Not sorted by mass");
+          .catch(e => {
+            console.log(e);
+            done(e);
           });
       });
 
-      it("should be able to sort the first 10 people by height", function() {
-        return axios
+      it("should be able to sort the first 10 people by height", function(done) {
+        axios
           .get(`${baseUrl}/people/?sort=height`)
           .then(response => {
             let people = response.data.results.map(person => {
               return person.name;
-            });
-            let sortedPeople = [
-              "R2-D2",
-              "R5-D4",
-              "Leia Organa",
-              "Beru Whitesun lars",
-              "C-3PO",
-              "Luke Skywalker",
-              "Owen Lars",
-              "Obi-Wan Kenobi",
-              "Biggs Darklighter",
-              "Darth Vader"
-            ];
+            }).slice(0,10);
+
+            let sortedPeople = ["Yoda", "Ratts Tyerell", "Wicket Systri Warrick", "Dud Bolt", "R2-D2", "R4-P17", "R5-D4", "Sebulba", "Gasgano", "Watto"];
+
             expect(sortedPeople).to.eql(people);
-            expect(response.headers["content-type"]).to.equal(
-              "application/json; charset=utf-8"
-            );
+            expect(response.headers["content-type"]).to.equal("application/json; charset=utf-8");
+            done();
           })
-          .catch(() => {
-            throw new Error("Not sorted by height");
+          .catch(e => {
+            console.log(e);
+            done(e);
           });
       });
 
-      it("should be able to sort the first 10 people by name alphabetically in REVERSE ORDER", function() {
-        return axios
+      it("should be able to sort the people by name alphabetically in REVERSE ORDER", function(done) {
+        axios
           .get(`${baseUrl}/people/?sort=name&reverseOrder=true`)
           .then(response => {
             let { data } = response;
@@ -136,52 +117,57 @@ describe("api endpoints", function() {
             });
             // copy the array, sort it, then reverse
             // then compare it to original
-            let sortedPeople = people.slice(0).sort().reverse();
+            let sortedPeople = people
+              .slice(0)
+              .sort()
+              .reverse();
             expect(sortedPeople).to.eql(people);
             expect(response.headers["content-type"]).to.equal("application/json; charset=utf-8");
+            done();
           })
-          .catch(() => {
-            throw new Error("Not sorted by name");
+          .catch(e => {
+            console.log(e);
+            done(e);
           });
       });
     });
   });
 
   describe("planets", function() {
-    it("should be able to fetch planet #1", function() {
-      return axios
+    it("should be able to fetch planet #1", function(done) {
+      axios
         .get(`${baseUrl}/planets/1`)
         .then(response => {
           let { data } = response;
-          expect(response.headers["content-type"]).to.equal(
-            "application/json; charset=utf-8"
-          );
+          expect(response.headers["content-type"]).to.equal("application/json; charset=utf-8");
           expect(data.name).to.be.equal("Tatooine");
           expect(data.name).to.not.equal("Star Wars");
+          done();
         })
-        .catch(() => {
-          throw new Error("Unable to fetch planet #1");
+        .catch(e => {
+          console.log(e);
+          done(e);
         });
     });
 
-    it("should be able to fetch planet #2 and show resident names instead of URLs", function() {
+    it("should be able to fetch planet #2 and show resident names instead of URLs", function(done) {
       let residents = ["Leia Organa", "Bail Prestor Organa", "Raymus Antilles"];
-      return axios
+      axios
         .get(`${baseUrl}/planets/2/residents`)
         .then(response => {
           let { data } = response;
           expect(data.residents).to.eql(residents);
-          expect(response.headers["content-type"]).to.equal(
-            "application/json; charset=utf-8"
-          );
+          expect(response.headers["content-type"]).to.equal("application/json; charset=utf-8");
+          done();
         })
-        .catch(() => {
-          throw new Error("Unable to fetch planet #2 with resident names");
+        .catch(e => {
+          console.log(e);
+          done(e);
         });
     });
 
-    it("should be able to fetch the first 10 planets", function() {
-      return axios
+    it("should be able to fetch all the planets", function(done) {
+      axios
         .get(`${baseUrl}/planets`)
         .then(response => {
           let { data } = response;
@@ -197,16 +183,18 @@ describe("api endpoints", function() {
           expect(planets)
             .to.be.an("array")
             .that.does.not.include("Hoath");
+          done();
         })
-        .catch(() => {
-          throw new Error("Unable to fetch first 10 planets");
+        .catch(e => {
+          console.log(e);
+          done(e);
         });
     });
 
     // ['name', 'population', 'diameter']
     describe("sorting", function() {
-      it("should be able to sort the first 10 planets by name alphabetically", function() {
-        return axios
+      it("should be able to sort the planets by name alphabetically", function(done) {
+        axios
           .get(`${baseUrl}/planets/?sort=name`)
           .then(response => {
             let { data } = response;
@@ -221,74 +209,88 @@ describe("api endpoints", function() {
             expect(response.headers["content-type"]).to.equal(
               "application/json; charset=utf-8"
             );
+            done();
           })
-          .catch(() => {
-            throw new Error("Not sorted by name");
+          .catch(e => {
+            console.log(e);
+            done();
           });
       });
 
-      it("should be able to sort the first 10 planets by population", function() {
-        return axios
+      it("should be able to sort the first 10 planets by population", function(done) {
+        axios
           .get(`${baseUrl}/planets/?sort=population`)
           .then(response => {
             let { data } = response;
-            let planets = data.results.map(planet => {
-              return planet.name;
-            });
+            let planets = data.results
+              .map(planet => {
+                return planet.name;
+              })
+              .slice(0, 10);
+
             let sortedPlanets = [
+              "Tund",
               "Yavin IV",
+              "Dantooine",
+              "Dathomir",
+              "Mustafar",
+              "Tatooine",
+              "Haruun Kal",
+              "Polis Massa",
               "Bespin",
-              "Endor",
-              "Kamino",
-              "Alderaan",
-              "Naboo",
-              "Geonosis",
-              "Coruscant",
-              "Hoth",
-              "Dagobah"
+              "Felucia"
             ];
+
             expect(sortedPlanets).to.eql(planets);
             expect(response.headers["content-type"]).to.equal(
               "application/json; charset=utf-8"
             );
+            done();
           })
-          .catch(() => {
-            throw new Error("Not sorted by population");
+          .catch(e => {
+            console.log(e);
+            done(e);
           });
       });
 
-      it("should be able to sort the first 10 planets by diameter", function() {
-        return axios
+      it("should be able to sort the first 10 planets by diameter", function(done) {
+        axios
           .get(`${baseUrl}/planets/?sort=diameter`)
           .then(response => {
             let { data } = response;
-            let planets = data.results.map(planet => {
-              return planet.name;
-            });
+            let planets = data.results
+              .map(planet => {
+                return planet.name;
+              })
+              .slice(0, 10);
+
             let sortedPlanets = [
+              "Stewjon",
+              "Cato Neimoidia",
+              "Polis Massa",
+              "unknown",
+              "Trandosha",
+              "Socorro",
+              "Mustafar",
               "Endor",
-              "Hoth",
-              "Dagobah",
-              "Yavin IV",
-              "Geonosis",
-              "Naboo",
-              "Coruscant",
-              "Alderaan",
-              "Kamino",
-              "Bespin"
+              "Bestine IV",
+              "Hoth"
             ];
+
             expect(sortedPlanets).to.eql(planets);
             expect(response.headers["content-type"]).to.equal(
               "application/json; charset=utf-8"
             );
+            done();
           })
-          .catch(() => {
-            throw new Error("Not sorted by diameter");
+          .catch(e => {
+            console.log(e);
+            done(e);
           });
       });
 
-      it("should be able to sort the first 10 planets by name alphabetically in REVERSE ORDER", function() {
-        return axios
+      it("should be able to sort the planets by name alphabetically in REVERSE ORDER", function(done) {
+        axios
           .get(`${baseUrl}/planets/?sort=name&reverseOrder=true`)
           .then(response => {
             let { data } = response;
@@ -297,12 +299,19 @@ describe("api endpoints", function() {
             });
             // copy the array, sort it, then reverse
             // then compare it to original
-            let sortedPlanets = planets.slice(0).sort().reverse();
+            let sortedPlanets = planets
+              .slice(0)
+              .sort()
+              .reverse();
             expect(sortedPlanets).to.eql(planets);
-            expect(response.headers["content-type"]).to.equal("application/json; charset=utf-8");
+            expect(response.headers["content-type"]).to.equal(
+              "application/json; charset=utf-8"
+            );
+            done();
           })
-          .catch(() => {
-            throw new Error("Not sorted by name in REVERSE ORDER");
+          .catch(e => {
+            console.log(e);
+            done(e);
           });
       });
     });
